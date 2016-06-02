7b9a491f804028959857694d3f5c183f12570c67
abdalimran <abdalimran@gmail.com>
2016-06-02 21:23:43 +0600

    Updated with export result feature.

--- src/main/java/ParseWikipedia.java
+++ src/main/java/ParseWikipedia.java
@@ -3,6 +3,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.PrintWriter;
+
 /*
 * This is simple web scrapper which scraps the "In the news" section's headlines.
 * This scrapper uses jsoup library. (https://jsoup.org/)
@@ -10,12 +12,18 @@ import org.jsoup.select.Elements;
 
 public class ParseWikipedia{
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         final Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
         final Elements newsHeadlines = doc.select("#mp-itn b a");
+        final PrintWriter export = new PrintWriter("WikipediaHeadlines.txt");
 
-        for(Element headline : newsHeadlines)
-            System.out.println(headline.attr("title"));
+        for (Element headline : newsHeadlines){
+            String printresult=headline.attr("title")+"\n";
+            System.out.print(printresult);
+            export.write(printresult);
+        }
+
+        export.close();
     }
 
 }
